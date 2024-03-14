60752429f67c2d111d525f57cc9daa0709f58a11
Meriellen Silveira <merysilveira@gmail.com>
2024-03-14 18:17:04 -0300

    Tabs layout done

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,54 +5,35 @@ export function Content() {
   return (
   <div className={styles.wrapper}>
   <Tabs.Root className={styles.TabsRoot} defaultValue="tab1">
-    <Tabs.List className={styles.TabsList} aria-label="Manage your account">
-      <Tabs.Trigger className={styles.TabsTrigger} value="tab1">
-        Entrada
-      </Tabs.Trigger>
-      <Tabs.Trigger className={styles.TabsTrigger} value="tab2">
-        Saída
-      </Tabs.Trigger>
+    <Tabs.List className={styles.TabsList}>
+      <Tabs.Trigger className={styles.TabsTrigger} value="tab1">Entrada</Tabs.Trigger>
+      <Tabs.Trigger className={styles.TabsTrigger} value="tab2">Saída</Tabs.Trigger>
     </Tabs.List>
     <Tabs.Content className={styles.TabsContent} value="tab1">
-      <p className={styles.Text}>Make changes to your account here. Click save when you're done.</p>
       <fieldset className={styles.Fieldset}>
-        <label className={styles.Label} htmlFor="name">
-          Name
-        </label>
-        <input className={styles.Input} id="name" defaultValue="Pedro Duarte" />
+        <label className={styles.Label} htmlFor="numEntry">Número da placa:</label>
+        <input className={styles.Input} id="numEntry" placeholder="AAA-0000" />
       </fieldset>
-      <fieldset className={styles.Fieldset}>
-        <label className={styles.Label} htmlFor="username">
-          Username
-        </label>
-        <input className={styles.Input} id="username" defaultValue="@peduarte" />
-      </fieldset>
-      <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-        <button className={styles.Button}>Save changes</button>
+      <div className={styles.wrapperButton}>
+        <button className={`${styles.Button} ${styles.ButtonEntry}`}>Confirmar Entrada</button>
       </div>
     </Tabs.Content>
     <Tabs.Content className={styles.TabsContent} value="tab2">
-      <p className="Text">Change your password here. After saving, you'll be logged out.</p>
       <fieldset className={styles.Fieldset}>
-        <label className={styles.Label} htmlFor="currentPassword">
-          Current password
-        </label>
-        <input className={styles.Input} id="currentPassword" type="password" />
+        <label className={styles.Label} htmlFor="numExit">Número da placa:</label>
+        <input className={styles.Input} id="numExit" placeholder="AAA-0000" />
       </fieldset>
-      <fieldset className={styles.Fieldset}>
-        <label className={styles.Label} htmlFor="newPassword">
-          New password
-        </label>
-        <input className={styles.Input} id="newPassword" type="password" />
-      </fieldset>
-      <fieldset className={styles.Fieldset}>
-        <label className={styles.Label} htmlFor="confirmPassword">
-          Confirm password
-        </label>
-        <input className={styles.Input} id="confirmPassword" type="password" />
-      </fieldset>
-      <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-        <button className={styles.Button}>Change password</button>
+
+      <div className={styles.wrapperButton}>
+        <button className={`${styles.Button} ${styles.ButtonPayment}`}>Pagamento</button>
+      </div>
+
+      <div className={styles.wrapperButton}>
+        <button className={`${styles.Button} ${styles.ButtonExit}`}>Saída</button>
+      </div>
+
+      <div className={styles.wrapperButton}>
+        <a href="#" className={styles.Link}>Ver Histórico</a>
       </div>
     </Tabs.Content>
   </Tabs.Root>
